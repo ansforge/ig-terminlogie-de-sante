ac92d990433029617e72fc2ebfc5ce32faaa5190
Mael PRIOUR <101335975+M-Priour@users.noreply.github.com>
2025-02-22 16:42:38 +0100

    Create compare_display.sql

---
+++ input/sql/compare_display.sql
@@ -0,0 +1,21 @@
+(select   
+        CodeSystemList.Name fts_codeSystem_name, 
+        CodeSystemList.url fts_codeSystem_url, 
+        ValueSetList.url   fts_valueSet_url,
+        ValueSetList.Name   fts_valueSet_name,
+        ValueSet_codes.Code  fts_valueSet_code , 
+        ValueSet_codes.Display fts_valueSet_display,
+        web
+from    ValueSet_codes, 
+        ValueSetList, 
+        CodeSystemList,
+        Resources  
+where   ValueSet_codes.ValueSetUri = ValueSetList.url AND
+        ValueSet_codes.System = CodeSystemList.Url AND
+        ValueSetList.ResourceKey = Resources.key and 
+        ValueSetList.Name not like "%_All" and
+        ValueSetList.ViewType = 1 
+)  fts on
+ValueSetView.system = fts.fts_codeSystem_url
+and  ValueSetView.code = fts.fts_valueSet_code
+
