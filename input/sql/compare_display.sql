d1bc371cc7a29205e1a0d9650437d8d2e88dcd65
Mael PRIOUR <101335975+M-Priour@users.noreply.github.com>
2025-02-24 10:14:06 +0100

    Update compare_display.sql

--- input/sql/compare_display.sql
+++ input/sql/compare_display.sql
@@ -6,14 +6,15 @@ ValueSetView.system codeSystem_url,
 ValueSetView.id   valueSet_id      ,
 ValueSetView.url   valueSet_url      ,
 ValueSetView.name valueSet_name,
+ValueSetView.status valueSet_status,
 ValueSetView.code valueSet_code,
-ValueSetView.display  valueSet_display,
+replace(ValueSetView.display, X'0A', '\n') valueSet_display,
 
 fts_valueSet_code,
 fts_valueSet_display,
 
 CodeSystemView.code interne_code,
-CodeSystemView.display interne_display,
+replace(CodeSystemView.display, X'0A', '\n')  interne_display,
 CodeSystemView.designation1 interne_designation1,
 CodeSystemView.designation2 interne_designation2,
 CodeSystemView.designation3 interne_designation3
@@ -45,5 +46,6 @@ left join  CodeSystemView ON
 ValueSetView.system = CodeSystemView.url
 and ValueSetView.code = CodeSystemView.code
 
-
+where 
+ValueSetView.id ='1.2.250.1.213.1.1.5.538'
 order by valueSet_name,codeSystem_url
