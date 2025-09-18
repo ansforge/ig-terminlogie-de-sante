ff8a88afcfd23596e4a660e602ab72c64b3cff4a
Mael PRIOUR <101335975+M-Priour@users.noreply.github.com>
2025-09-18 08:44:12 +0200

    Update sql-add-info-codesystem.sql
    Ajout su status des terminologies dans la vue sql

--- input/sql/sql-add-info-codesystem.sql
+++ input/sql/sql-add-info-codesystem.sql
@@ -18,7 +18,8 @@
                         {  "path": "display", "name": "display" },      
                         {  "path": "designation[0].value", "name": "designation1" },
                         {  "path": "designation[1].value", "name": "designation2" } ,
-                        {  "path": "designation[2].value", "name": "designation3" }                        
+                        {  "path": "designation[2].value", "name": "designation3" }  ,
+                        {  "path": "property.where(code='status').valueCode", "name": "status"}
                 ]
                 
             }
