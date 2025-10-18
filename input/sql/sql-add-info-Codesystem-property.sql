3eaa0ee526b0c61ca5272c61eb91be40bec88b71
Mael PRIOUR <101335975+M-Priour@users.noreply.github.com>
2025-10-18 14:07:56 +0200

    Update sql-add-info-Codesystem-property.sql

--- input/sql/sql-add-info-Codesystem-property.sql
+++ input/sql/sql-add-info-Codesystem-property.sql
@@ -4,22 +4,22 @@
   "select": [
     {
         "column": [
-            { "name": "id","path": "id" , "type":"string"},
-            { "name": "name","path": "name", "type":"string"},
-            { "name": "title", "path": "title", "type":"string"},
-            { "name": "url", "path": "url" , "type":"string"},
-            { "name": "identifier", "path": "identifier.where(system='urn:ietf:rfc:3986')[0].value" , "type":"string"},  
-            { "name": "status", "path": "status", "type":"string"}
+            { "name": "id","path": "id"  },
+            { "name": "name","path": "name" },
+            { "name": "title", "path": "title" },
+            { "name": "url", "path": "url"  },
+            { "name": "identifier", "path": "identifier.where(system='urn:ietf:rfc:3986')[0].value"  },  
+            { "name": "status", "path": "status" }
         ] ,
         "select": [
             
                {
                 "forEachOrNull": "property",
                 "column": [ 
-                        {  "path": "code", "name": "codeP", "type":"string" },
-                        {  "path": "uri", "name": "uriP" , "type":"string"},      
-                        {  "path": "type", "name": "typeP", "type":"string" },
-                        {  "path": "description", "name": "descriptionP" , "type":"string"} 
+                        {  "path": "code", "name": "codeProperty"  },
+                        {  "path": "uri", "name": "uriProperty"  },      
+                        {  "path": "type", "name": "typeProperty"  },
+                        {  "path": "description", "name": "descriptionProperty"  } 
                 ]
                 
             }         
