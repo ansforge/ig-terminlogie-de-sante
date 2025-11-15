40e7f587bacc0fd7d55c31815b721c5da1cfffc3
mael priour <mael.priour@esante.gouv.fr>
2025-11-15 15:01:56 +0100

    MAj des vues sql

--- input/sql/sql-add-info-codesystem-property.sql
+++ input/sql/sql-add-info-codesystem-property.sql
@@ -1,34 +1,30 @@
 {
-  "name" : "CodeSystemPropertyView",
-  "resource" : "CodeSystem",
+  "name": "CodeSystemPropertyView",
+  "resource": "CodeSystem",
   "select": [
     {
-        "column": [
-            { "name": "id","path": "id"  },
-            { "name": "name","path": "name" },
-            { "name": "title", "path": "title" },
-            { "name": "url", "path": "url"  },
-            { "name": "identifier", "path": "identifier.where(system='urn:ietf:rfc:3986')[0].value"  },  
-            { "name": "status", "path": "status" }
-        ] ,
-        "select": [
-            
-               {
-                "forEachOrNull": "property",
-                "column": [ 
-                        {  "path": "code", "name": "codeProperty"  },
-                        {  "path": "uri", "name": "uriProperty"  },      
-                        {  "path": "type", "name": "typeProperty"  },
-                        {  "path": "description", "name": "descriptionProperty"  } 
-                ]
-                
-            }         
-            
- 
-        ]
+      "column": [
+        { "name": "id", "path": "id" },
+        { "name": "name", "path": "name" },
+        { "name": "title", "path": "title" },
+        { "name": "url", "path": "url" },
+        { "name": "identifier", "path": "identifier.where(system='urn:ietf:rfc:3986')[0].value" },
+        { "name": "status", "path": "status" }
+      ],
+      "select": [
+        {
+          "forEach": "property",
+          "column": [
+            { "path": "code.first()", "name": "codeProperty" },
+            { "path": "uri.first()", "name": "uriProperty" },
+            { "path": "type.first()", "name": "typeProperty" },
+            { "path": "description.first()", "name": "descriptionProperty" }
+          ]
+        }
+      ]
     }
-],
-  "where"  : [{
-    "path" : "CodeSystem.url.exists()"
+  ],
+  "where": [{
+    "path": "CodeSystem.url.exists()"
   }]
 }
